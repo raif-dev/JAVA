80e05916fa29a44deb348941de3a52c9bba7ceb4
Java Practice <java-practice@example.com>
2026-09-25 12:03:58 +0530

    Add Java type conversion practice files

---
+++ typeConversion/Test2.java
@@ -0,0 +1,10 @@
+public class Test2 {
+    public static void main ( String args[] )
+    {
+        char ch='A';
+        int num=ch+2;
+        System.out.println((int)num);
+        System.out.println((char)num);
+    }
+    
+}
---
+++ typeConversion/Test3.java
@@ -0,0 +1,8 @@
+ public class Test3 {
+    public static void main(String[] args) {
+        short a=10;
+        short b=20;
+        int c=a+b;
+        System.out.println(c);
+    }
+}
